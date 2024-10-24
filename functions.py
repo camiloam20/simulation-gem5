7552a3d25b891465e08815cf218cc58054616cdd
Camilo Alvarez Muñoz <camilo.alvarezm@udea.edu.co>
2024-10-24 17:20:42 -0500

    Codigo final

--- functions.py
+++ functions.py
@@ -3,22 +3,27 @@ import time
 import itertools
 import csv
 import os
+import random
 
+#Funciones:
+#Camilo Alvarez Muñoz
+#Daniel Cano Restrepo
 
-
+def lista_aleatoria(tamano):
+    #Funcion para generar un estado aleatorio de la DSE para utilizar como punto inicial del recocido simulado
+    return [random.randint(0, 2) for _ in range(tamano)]
 
 def run_bash_command(command):
+    #Ejecuta un comando de bash en el sistema
     process = subprocess.Popen(command, shell=True)
     return process
 
-
-
 def are_processes_running(processes):
+    #Verifica si los procesos de bash siguen ejecutandose
     return any(process.poll() is None for process in processes)
 
-
-
 def mcpat():
+    #Esta función ejecuta McPAT para extraer los resultados de potencia y energia de cada programa
     programs = [
     "h264_dec",
     "h264_enc",
@@ -27,14 +32,8 @@ def mcpat():
     "mp3_dec",
     "mp3_enc"
     ]
-    #if ejemplo!=0:
-    #    print("esto es una prueba")
-    #    comands = f"python3 ../scripts/McPAT/gem5toMcPAT_cortexA76.py {programs[0]}/stats.txt {programs[0]}/config.json ../scripts/McPAT/ARM_A76_2.1GHz.xml"
-    #    mcpat_commands = "./../mcpat/mcpat -infile ../mcpat/ProcessorDescriptionFiles/ARM_A9_2GHz_withIOC.xml "
-    #    subprocess.run(comands + "&&" + mcpat_commands+ f"> energy.txt", shell=True )
-    #return False
 
-    
+    #Lista de comandos para ejecutar McPAT para cada programa
     mcpatpy_commands = [
         f"python3 ../scripts/McPAT/gem5toMcPAT_cortexA76.py {programs[0]}/stats.txt {programs[0]}/config.json ../scripts/McPAT/ARM_A76_2.1GHz.xml",
         f"python3 ../scripts/McPAT/gem5toMcPAT_cortexA76.py {programs[1]}/stats.txt {programs[1]}/config.json ../scripts/McPAT/ARM_A76_2.1GHz.xml",
@@ -43,21 +42,16 @@ def mcpat():
         f"python3 ../scripts/McPAT/gem5toMcPAT_cortexA76.py {programs[4]}/stats.txt {programs[4]}/config.json ../scripts/McPAT/ARM_A76_2.1GHz.xml",
         f"python3 ../scripts/McPAT/gem5toMcPAT_cortexA76.py {programs[5]}/stats.txt {programs[5]}/config.json ../scripts/McPAT/ARM_A76_2.1GHz.xml"
     ]
-    #  ./mcpat -infile <*.xml>  -print_level < level of detailed output>
-    #  ./mcpat -h (or mcpat --help) will show the quick help message.
+    #./mcpat -infile <*.xml>  -print_level < level of detailed output>
     mcpat_commands = "./../mcpat/mcpat -infile config.xml"
     i=0
+    #Ejecuta en primer lugar el archivo de python para generar el archivo config.xml y luego ejecuta McPAT con este mismo archivo
     for comands in mcpatpy_commands:
         subprocess.run(comands, shell=True )
         subprocess.run(mcpat_commands+ f"> {programs[i]}/energy.txt", shell=True )
         i+=1
 
-
-
 def extract_potencia(nombre_archivo):
-
-
-
     # Inicializar variables para almacenar los totales
     total_leakage = 0.0
     runtime_dynamic = None  # Inicializamos como None para capturar solo el primer valor
@@ -91,15 +85,10 @@ def extract_potencia(nombre_archivo):
     # Realizar la operación
     resultado = total_leakage + runtime_dynamic
 
-    # Imprimir el resultado
-    #print(f"Total leakage: {total_leakage}")
-    #print(f"Runtime Dynamic: {runtime_dynamic}")
-    #print(f"Suma (Total leakage + Runtime Dynamic): {resultado}")
     return resultado
 
-
-
 def extract_stats(file_path):
+    #Esta función lee los archivos stats.txt de cada programa y extrae los valores de simInsts, system.cpu.cpi y system.cpu.ipc
     simInsts = None
     system_cpu_cpi = None
     system_cpu_ipc = None
@@ -118,9 +107,8 @@ def extract_stats(file_path):
 
     return simInsts, system_cpu_cpi, system_cpu_ipc
 
-
-
 def eval_state(estado):
+    #Lista de los programas a ejecutar
     programs = [
     "h264_dec",
     "h264_enc",
@@ -130,13 +118,30 @@ def eval_state(estado):
     "mp3_enc"
     ]
 
-    parametros=["--l1i_size=","--num_fu_intALU=","--num_fu_read=","--num_fu_write=","--num_fu_FP_SIMD_ALU="]
-    parametro0=["64kB ","128kB ","256kB ","512kB"]
-    parametro1=["2 ","4 ","6 ","8 "]
-    parametro2=["2 ","4 ","6 ","8 "]
-    parametro3=["2 ","4 ","6 ","8 "]
-    parametro4=["1 ","2 ","4 ","8 "]
+    #Espacio de diseño: 
+    #Parametros a modificar del procesador
+    parametros=[
+        "--num_fu_intALU=",
+        "--num_fu_read=",
+        "--num_fu_write=",
+        "--num_fu_FP_SIMD_ALU=",
+        "--l1i_size=",
+        "--l1i_assoc=",
+        "--l1d_size=",
+        "--l1d_assoc="
+        ]
+    
+    #Valores de cada parametro:
+    parametro0=["2 ","4 ","6 "]#Numero de unidades funcionales para las instrucciones de ALU enteros 
+    parametro1=["2 ","4 ","6 "]#Numero de unidades funcionales para las instrucciones Load
+    parametro2=["2 ","4 ","6 "]#Numero de unidades funcionales para las instrucciones store
+    parametro3=["1 ","2 ","4 "]#Numero de unidades funcionales para las instrucciones float y SIMD
+    parametro4=["64kB ","128kB ","256kB "]#Tamaño de la cache de instrucciones L1
+    parametro5=["4 ","8 ","16 "]#Asociatividad de la cache de instrucciones L1
+    parametro6=["64kB ","128kB ","256kB "]#Tamaño de la cache de datos L1
+    parametro7=["4 ","8 ","16 "]#Asociatividad de la cache de datos L1
 
+    #Rutas de los programas a ejecutar en la carpeta workloads
     programs_path = [
         "../workloads/h264_dec/",
         "../workloads/h264_enc/",
@@ -146,6 +151,7 @@ def eval_state(estado):
         "../workloads/mp3_enc/"
     ]
     
+    #Opciones adicionales de los programas a ejecutar
     program_options = [
         f"{programs_path[0]}h264dec_testfile.264 {programs_path[0]}h264dec_outfile.yuv",
         f"{programs_path[1]}h264enc_configfile.cfg -org {programs_path[1]}h264enc_testfile.yuv -bf {programs_path[1]}h264enc_testfile.264 ",
@@ -154,10 +160,13 @@ def eval_state(estado):
         f"-w {programs_path[4]}mp3dec_outfile.wav {programs_path[4]}mp3dec_testfile.mp3",
         f"{programs_path[5]}mp3enc_testfile.wav {programs_path[5]}mp3enc_outfile.mp3",
     ]
+    #Ruta del gem5.fast
     gem5_path = "../gem5/build/ARM"
+    #Ruta del script de python para parametros del procesador CortexA76
     python_cortex_path = "../scripts/CortexA76_scripts_gem5"
-    parametros_str = parametros[0] + parametro0[estado[0]] + parametros[1] + parametro1[estado[1]] + parametros[2] + parametro2[estado[2]] + parametros[3] + parametro3[estado[3]] + parametros[4] + parametro4[estado[4]]
+    parametros_str = parametros[0] + parametro0[estado[0]] + parametros[1] + parametro1[estado[1]] + parametros[2] + parametro2[estado[2]] + parametros[3] + parametro3[estado[3]] + parametros[4] + parametro4[estado[4]] + parametros[5] + parametro5[estado[5]] + parametros[6] + parametro6[estado[6]] + parametros[7] + parametro7[estado[7]]
 
+    #Comandos de bash para ejecutar los programas en gem5
     bash_commands = [
         f"{gem5_path}/gem5.fast -d {programs[0]} {python_cortex_path}/CortexA76.py --cmd={programs_path[0]+programs[0]} --options=\"{program_options[0]}\" {parametros_str} && echo 'Terminó el proceso 1'",
         f"{gem5_path}/gem5.fast -d {programs[1]} {python_cortex_path}/CortexA76.py --cmd={programs_path[1]+programs[1]} --options=\"{program_options[1]}\" {parametros_str} && echo 'Terminó el proceso 2'",
@@ -168,6 +177,7 @@ def eval_state(estado):
     ]
 
     processes = []
+    #Este ciclo ejecuta todos los programas bash de la lista de forma simultanea
     for command in bash_commands:
         processes.append(run_bash_command(command))
         time.sleep(5)
@@ -176,6 +186,7 @@ def eval_state(estado):
 
     seg = 0
     minutes = 0
+    #Este ciclo verifica si los procesos de bash han terminado, cuando acabe la simulación de mayor tiempo se saldrá del ciclo
     while are_processes_running(processes):
         if seg == 60:
             minutes += 1
@@ -186,12 +197,12 @@ def eval_state(estado):
 
     print("Todos los procesos han terminado.")
 
-
-
 def save_new_data(estado):
 
+    #Llamamos la función para ejecutar McPAT y extraer los resultados de potencia y energia de cada programa
     mcpat()
 
+    #Lista de los programas a ejecutar
     programs = [
     "h264_dec",
     "h264_enc",
@@ -211,7 +222,7 @@ def save_new_data(estado):
     if not os.path.exists(results_file):
         with open(results_file, mode="a", newline="") as file:
             csv_writer = csv.writer(file)
-            csv_writer.writerow(["Experimento", "Programa", "Parametro 1", "Parametro 2", "Parametro 3", "Parametro 4", "Parametro 5", "simInsts", "system_cpu_cpi", "system_cpu_ipc", "CPUtime","power","EDP"])  # Escribir los encabezados
+            csv_writer.writerow(["Experimento", "Programa", "Parametro 1", "Parametro 2", "Parametro 3", "Parametro 4", "Parametro 5", "Parametro 6", "Parametro 7", "Parametro 8", "simInsts", "system_cpu_cpi", "system_cpu_ipc", "CPUtime","power","EDP"])  # Escribir los encabezados
 
     # Verificar si el archivo de promedios existe y escribir encabezados si no
     if not os.path.exists(avg_results_file):
@@ -232,6 +243,7 @@ def save_new_data(estado):
     total_EDP = 0
     total_energy = 0
      
+    #Iterar sobre las carpetas de resultados de los 6 programas
     for i in range(6):
         simInsts, system_cpu_cpi, system_cpu_ipc = extract_stats(f"{programs[i]}/stats.txt")
         energy = extract_potencia(f"{programs[i]}/energy.txt")
@@ -251,26 +263,27 @@ def save_new_data(estado):
         EDP = energy*system_cpu_cpi
         total_EDP += EDP
 
-        if i == 5:  # En el sexto programa, escribir promedios en columnas "avg_cpi" y "avg_ipc"
+        if i == 5:  # En el sexto programa, escribir promedios en el archivo avgResults
             avg_cpi = total_cpi / 6
             avg_ipc = total_ipc / 6
             avg_cpu = total_cput / 6
             avg_energy = total_energy / 6
             avg_EDP = total_EDP / 6
 
+            #Escribir los resultados de cada simulación en el archivo de resultados
             with open(results_file, mode="a", newline="") as file:
                 csv_writer = csv.writer(file)
-                csv_writer.writerow([experimento, programs[i], estado[0], estado[1], estado[2], estado[3], estado[4], simInsts, system_cpu_cpi, system_cpu_ipc, cpu_time, energy , EDP])
+                csv_writer.writerow([experimento, programs[i], estado[0], estado[1], estado[2], estado[3], estado[4], estado[5], estado[6], estado[7], simInsts, system_cpu_cpi, system_cpu_ipc, cpu_time, energy , EDP])
             
+            #Escribir los promedios al momento de tener los resultados de los 6 programas
             with open(avg_results_file, mode="a", newline="") as avg_file:
                 avg_csv_writer = csv.writer(avg_file)
-                avg_csv_writer.writerow([experimento, avg_cpi, avg_ipc, avg_cpu , avg_energy, avg_EDP])  # Guardar avg_cpi y avg_ipc en el nuevo CSV
+                avg_csv_writer.writerow([experimento, avg_cpi, avg_ipc, avg_cpu , avg_energy, avg_EDP])
         
         else:
-
             with open(results_file, mode="a", newline="") as file:
                 csv_writer = csv.writer(file)
-                csv_writer.writerow([experimento, programs[i], estado[0], estado[1], estado[2], estado[3], estado[4], simInsts, system_cpu_cpi, system_cpu_ipc, cpu_time, energy, EDP])
+                csv_writer.writerow([experimento, programs[i], estado[0], estado[1], estado[2], estado[3], estado[4], estado[5], estado[6], estado[7], simInsts, system_cpu_cpi, system_cpu_ipc, cpu_time, energy, EDP])
          
-    return avg_EDP
+    return avg_EDP#Retorna el EDP promedio que servirá como función objetivo
 
--- script.py
+++ script.py
@@ -3,6 +3,10 @@ from functions import *
 import math
 import random
 
+#Script principal - Recocido simulado para la simulación de microarquitecturas de procesadores
+#Camilo Alvarez Muñoz
+#Daniel Cano Restrepo
+
 # Función objetivo que queremos minimizar (ajustar según el problema)
 def funcion_objetivo(estado):
     eval_state(estado)
@@ -19,7 +23,7 @@ def generar_nuevo_punto(estado,n=5):
         # Aplicar la operación al valor del índice
         if (estado[indice]==0):
             estado[indice] +=1
-        elif (estado[indice]==4):
+        elif (estado[indice]==2):
             estado[indice] -=1
         else:
             estado[indice] += operacion
@@ -33,25 +37,29 @@ def generar_nuevo_punto(estado,n=5):
 # Parámetros del recocido simulado
 def recocido_simulado(funcion_objetivo, generar_nuevo_punto, temperatura_inicial, factor_enfriamiento, iteraciones):
     # Punto inicial (ajustar según el problema)
-    x_actual = [0,0,0,0,0]
+    x_actual = lista_aleatoria(8)
     mejor_x = x_actual
+    print("\n\n\n\n\n","estado: ",x_actual,"\n\n\n\n\n")
     mejor_valor = funcion_objetivo(x_actual)
+    valor_actual = mejor_valor
     
     temperatura = temperatura_inicial
     
     for i in range(iteraciones):
         # Generar un nuevo punto y calcular su valor
         nuevo_x = generar_nuevo_punto(x_actual)
-        valor_actual = funcion_objetivo(x_actual)
+        print("\n\n\n\n\n","estado: ",x_actual,"\n\n\n\n\n")
         nuevo_valor = funcion_objetivo(nuevo_x)
         
         # Calcula la diferencia de energía
-        delta = nuevo_valor - valor_actual
+        delta = (nuevo_valor - valor_actual)*10000
         
         # Si el nuevo valor es mejor, aceptamos el cambio
         # O si es peor, lo aceptamos con una probabilidad que depende de la temperatura
+        print("\n\n\n\n\n","probabilidad de cambio: ",math.exp(-delta / temperatura),"\n\n\n\n\n")
         if delta < 0 or random.random() < math.exp(-delta / temperatura):
             x_actual = nuevo_x
+            valor_actual = nuevo_valor
             
             # Actualizar el mejor valor encontrado
             if nuevo_valor < mejor_valor:
@@ -65,12 +73,14 @@ def recocido_simulado(funcion_objetivo, generar_nuevo_punto, temperatura_inicial
 
 # Ajuste de los parámetros
 probabilidad_cambio = 0.9  # Probabilidad inicial del 90%
-delta_inicial = 2.0  # Supongamos un cambio inicial de 1.0 en la función objetivo
+delta_inicial = 0.1*10000  # Cambio inicial en la función objetivo
 temperatura_inicial = -delta_inicial / math.log(probabilidad_cambio)  # Ajusta la temperatura para un 90% de probabilidad
-factor_enfriamiento = 0.7  # Ajusta la velocidad de enfriamiento
-iteraciones = 14  # Ajusta el número de iteraciones
+print(temperatura_inicial)
+factor_enfriamiento = 0.9  # Ajusta la velocidad de enfriamiento
+iteraciones = 50  # Ajustar el número de iteraciones según las microarquitecturas a analizar
 
 # Ejecutar el recocido simulado
 mejor_x, mejor_valor = recocido_simulado(funcion_objetivo, generar_nuevo_punto, temperatura_inicial, factor_enfriamiento, iteraciones)
 
+# Imprimir los mejores parametros de microarquitectura encontrados en el DSE
 print(f"Mejor solución encontrada: x = {mejor_x}, valor = {mejor_valor}")
